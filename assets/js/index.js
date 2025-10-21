54d8cb9166f6a548b7fd033e9c78815f7376d18d
Minhaj Haider Shah <smhaider1808@gmail.com>
2025-10-21 16:51:28 +0500

    fixes

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+const sidebar = document.getElementById("dashborad-sidebar");
+const main = document.querySelector(".bar-link-col");
+const collapses = document.querySelectorAll(".collapse");
+document.getElementById("search-app-open").addEventListener("click", function () {
+  collapses.forEach((clp) => {
+    clp.classList.remove("show");
+  });
+  main.classList.toggle("collapsed");
+  sidebar.classList.toggle("collapsed");
+});
+const SidebarcollapseBtn = document.querySelectorAll(".nav-main-link");
+
+SidebarcollapseBtn.forEach((clp) => {
+  clp.addEventListener("click", function () {
+    if (main.classList.contains("collapsed") && sidebar.classList.contains("collapsed")) {
+      main.classList.toggle("collapsed");
+      sidebar.classList.toggle("collapsed");
+    }
+  });
+});
--- index.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("search-app-open").addEventListener("click", function () {
-    const sidebar = document.getElementById("dashborad-sidebar");
-    sidebar.classList.toggle("collapsed");
-   
-
-    
-  });
-  
