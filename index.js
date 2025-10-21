ceb5cf614c53a86198c9f9567a52434e08e5e452
furqan <furqanmayo797@gmail.com>
2025-10-21 02:51:42 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+document.getElementById("search-app-open").addEventListener("click", function () {
+    const sidebar = document.getElementById("dashborad-sidebar");
+    sidebar.classList.toggle("collapsed");
+   
+
+    
+  });
+  
