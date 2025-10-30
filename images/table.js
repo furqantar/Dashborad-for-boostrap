da9fe04e1471a940d7bc3440260c2f7c98e9195d
furqan <furqanmayo797@gmail.com>
2025-10-30 05:22:40 -0700

    change update all

---
+++ images/table.js
@@ -0,0 +1,24 @@
+const sidebar = document.getElementById("dashborad-sidebar");
+const main = document.querySelector(".bar-link-col");
+const collapses = document.querySelectorAll(".collapse");
+document.getElementById("search-app-open").addEventListener("click", function () {
+  collapses.forEach((clp) => {
+    clp.classList.remove("show");
+  });
+  main.classList.toggle("collapsed");
+  sidebar.classList.toggle("collapsed");
+});
+const SidebarcollapseBtn = document.querySelectorAll(".nav-main-link");
+
+SidebarcollapseBtn.forEach((clp) => {
+  clp.addEventListener("click", function () {
+    if (main.classList.contains("collapsed") && sidebar.classList.contains("collapsed")) {
+      main.classList.toggle("collapsed");
+      sidebar.classList.toggle("collapsed");
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+  const table = new DataTable('#myTable');
+})
